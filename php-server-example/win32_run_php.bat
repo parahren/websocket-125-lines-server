c0c0ce88a72f258676e4653adcee5d2b9467647a
Pär Ahrén <par.ahren@infrasec.se>
2014-03-17 20:44:58 +0100

    Update win32_run_php.bat

--- php-server-example/win32_run_php.bat
+++ php-server-example/win32_run_php.bat
@@ -1,6 +1,6 @@
-@rem echo off
+@echo off
 @title Websocket server in PHP by par.ahren@infrasec.se
-@echo Websocket server in PHP by par.ahren@infrasec.se
+@echo Starting PHP-Websocket server
 
 php  scr\ws_auth-standalone.php
 
